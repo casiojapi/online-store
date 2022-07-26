d71b1f27bc7b235fd2063a93ba03d5dcda6fb67c
casiojapi <maximosantoro@protonmail.com>
2022-07-26 20:29:20 -0300

    Initial commit

---
+++ src/main/java/maximo/san/onlineStore/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package maximo.san.onlineStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
